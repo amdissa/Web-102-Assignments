2a827b30bfc565e6d01270b9feded05d856aef69
nazrin <nazrin@berkeley.edu>
2025-06-12 11:00:48 -0700

    Final Calendar with events

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,13 +1,15 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 /**
  * Root file App.jsxn
  */
 
 import './App.css';
+import Calendar from './components/Calendar'
+
 
 const App = () => {
 
@@ -17,6 +19,7 @@ const App = () => {
       <h2>
         Welcome to Chicago, Amdissa! Check out this calendar to get to know the city and see all the sights during your stay.
       </h2>
+      <Calendar> </Calendar>
       
     </div>
   )
---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import Event from './Event'
+
+const Calendar = () => {
+  return (
+    <div className="Calendar"> 
+    <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+            <th>Sunday</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="time">8 am</td>
+             <Event event='Starbucks ☕️' color='green' location='646 Michigan Ave' />
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event='Yolk 🍳' color='green' location='355 E Ohio St' />
+            <td></td>
+            <td>
+            <Event event='Subway 🚇' color='pink' location='Grand Station' />
+
+            </td>
+          </tr>
+          <tr>
+            <td className="time">9 am</td>
+            <td></td>
+            <Event event='River Cruise ⛵' color='blue' location='Chicago River' />
+
+            <td></td><td><Event event='Subway 🚇' color='pink' location='Grand Station' />
+            </td><td></td><td></td><td></td><td></td>
+          </tr>
+          <tr>
+            <td className="time">10 am</td>
+            <td></td>
+            <Event event='The Coffee Bean 🫘' color ='blue'/>
+            <td></td><td></td><td></td><td></td><td></td><td></td>
+          </tr>
+          <tr>
+            <td className="time">11 am</td>
+            <td></td><td></td>
+            <Event event='Deep Dish 🍕' color='green' location="Giordano's" />
+
+            <td></td><td></td><td></td><td></td><td></td>
+          </tr>
+          <tr>
+            <td className="time">12 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">1 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event='Girl & the Goat' color='green' location='809 W Randolph St' />
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">2 pm</td>
+            <td></td>
+            <Event event='Art Institute 🖼️' color='blue' location='111 S Michigan Ave' />
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event='Subway 🚇' color='pink' location='Roosevelt Station' />
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">3 pm</td>
+            <td></td>
+            <Event event='Cubs Game ⚾' color='blue' location='Wrigley Field' />
+            <td></td>
+            <td></td>
+            <Event event='Deep Dish 🍕' color='green' location="Giordano's" />
+
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">4 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event event='Fancy Dinner 🎩' color='green' location='Maple & Ash' />
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">5 pm</td>
+              <Event event='Fancy Dinner 🎩' color='green' location='Maple & Ash' />
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+              <Event event='Shopping 🛍️' color='blue' location='Magnificent Mile' />
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Calendar;
